c9ed12d734662567f884132f2501f9dbe9ab74e9
taketyan-man <119859981+taketyan-man@users.noreply.github.com>
2024-04-10 07:15:22 +0000

    rails tutoliarl 9.3.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,14 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
