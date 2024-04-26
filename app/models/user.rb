deed09bb86c8af4f16f00158855d33412e86d898
taketyan-man <119859981+taketyan-man@users.noreply.github.com>
2024-04-26 06:53:56 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,10 +74,13 @@ class User < ApplicationRecord
     self.reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
-  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", self.id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+                    .includes(:user, image_attachment: :blob)
   end
 
   # ユーザーをフォローする
