ae2b6a4c855b6a309805311075e089430c4de72e
taketyan-man <119859981+taketyan-man@users.noreply.github.com>
2024-05-08 14:15:22 +0000

    delete test add

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -6,6 +6,29 @@ RSpec.describe "Sessions", type: :request do
       get login_path
       expect(response).to have_http_status :ok
     end
-    
   end
+
+  describe 'DELETE /logout' do
+    before do
+      user = FactoryBot.create(:user)
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password } }
+    end
+
+    it 'should valid logout' do
+      user = FactoryBot.create(:user)
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password } }
+      expect(logged_in?).to be_truthy
+ 
+      delete logout_path
+      expect(logged_in?).to_not be_truthy
+    end
+
+    it 'should still work after logout in second window' do
+      delete logout_path
+      delete logout_path
+      expect(response).to redirect_to root_path
+    end
+   end
 end
